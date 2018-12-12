30fe58901d4a3675f537600935e2d5bb317264d6
nstco29 <nstco29@gmail.com>
2018-12-12 09:52:34 -0500

    adjustment in class w/danny

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,91 +1,21 @@
 'use strict'
 
-const config = require('./config.js')
-const store = require('./store.js')
+// use require with a reference to bundle the file and use it in this file
+// const example = require('./example')
 
-const game = () => {
-  return $.ajax({
-    url: config.apiUrl + '/games',
-    headers: {
-      Authorization: 'Token token=' + store.user.token
-    },
-    method: 'POST',
-  })
-}
+// use require without a reference to ensure a file is bundled
+// require('./example')
 
-const userMoves = function (index, value, over) {
-  return $.ajax({
-    url: config.apiUrl + '/games/:id',
-    method: 'PATCH',
-    headers: {
-      Authorization: 'Token token=' + store.user.token
-    },
-    data: {
-      'game': {
-        'cell': {
-          'index': index,
-          'value': value
-        },
-        'over': over
-      }
-    }
-  })
-}
+const authEvents = require('./event.js')
 
-const gameHistory = () => {
-  return $.ajax({
-    url: config.apiUrl + '/games',
-    method: 'GET',
-    headers: {
-      Authorization: 'Token token=' + store.user.token
-    }
-  })
-}
+const JS = require('./JS.js')
 
-const signUp = data => {
-  return $.ajax({
-    url: config.apiUrl + '/sign-up',
-    method: 'POST',
-    data: data
-  })
-}
 
-const signIn = data => {
-  return $.ajax({
-    url: config.apiUrl + '/sign-in',
-    method: 'POST',
-    data: data
-  })
-}
-
-const changePassword = data => {
-  return $.ajax({
-    url: config.apiUrl + '/change-password',
-    method: 'PATCH',
-    headers: {
-      Authorizaton: 'Token token=' + store.user.token
-    },
-    data: data
-  })
-}
-
-const signOut = () => {
-  return $.ajax({
-    url: config.apiUrl + '/sign-out',
-    method: 'DELETE',
-    headers: {
-      Authorization: 'Token token=' + store.user.token
-    }
-  })
-}
-
-module.exports = {
-  signUp,
-  signIn,
-  changePassword,
-  signOut,
-  game,
-  userMoves,
-  gameHistory,
-}
-
+$(() => {
+  // your JS code goes here
+  $('#sign-up').on('click', authEvents.onSignUp)
+  $('#sign-in').on('click', authEvents.onSignIn)
+  // $('#sign-out').on('click', authEvents.onSignOut)
+  $('#change-password').on('click', authEvents.onChangePassword)
+  $('#newGameButton').on('click', JS.beginGame)
+})
