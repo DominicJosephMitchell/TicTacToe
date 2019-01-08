d88ef7f9b346b097a08983269a46c4410bcf8a98
nstco29 <nstco29@gmail.com>
2019-01-07 20:57:36 -0500

    update

--- curl-scripts/update.sh
+++ curl-scripts/update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-curl --include --request PATCH "https://wdi-library-api.herokuapp.com/books/${ID}" \
-  --header "Content-type: application/json" \
-  --data '{
-    "bucketList": {
-      "title":"'"${TITLE}"'",
-      "subtitle":"'"${SUBTITLE}"'",
-      "description":"'"${DESCRIPTION}"'"
-    }
-  }'
+# curl --include --request PATCH "https://wdi-library-api.herokuapp.com/books/${ID}" \
+#   --header "Content-type: application/json" \
+#   --data '{
+#     "bucketList": {
+#       "title":"'"${TITLE}"'",
+#       "subtitle":"'"${SUBTITLE}"'",
+#       "description":"'"${DESCRIPTION}"'"
+#     }
+#   }'
 
-echo
+# echo
