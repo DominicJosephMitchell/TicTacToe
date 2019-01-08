2106eb3413109f70d7347db344814faf38117a9a
nstco29 <nstco29@gmail.com>
2019-01-08 12:27:34 -0500

    config

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -4,7 +4,7 @@ let apiUrl
 const apiUrls = {
   development: 'https://aqueous-atoll-85096.herokuapp.com',
   // development: 'http://localhost:4741',
-  production: 'https://tic-tac-toe-wdi.herokuapp.com'
+  production: 'https://tic-tac-toe-wdi.herokuapp.com/'
 }
 
 if (window.location.hostname === 'localhost') {
